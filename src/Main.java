377d41ff087ed3d85aed43bdab6721ee8ae74aea
andressgiraldor-maker <andress.giraldor@uqvirtual.edu.co>
2026-08-16 21:16:24 -0500

    subiendo cambios de dev_andressantiago

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 class Main{
     public static void main(String[] args){
+
         System.out.print("Hola soy, Alejandro Rodríguez Duque");
+
+        System.out.print("hola soy, andres santiago giraldo");
     }
 }
