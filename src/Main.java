86b1f6d72a4aa21c1a36c55519b997f1168c8e8d
Juan Salas <juand.salasa@uqvirtual.edu.co>
2026-08-17 00:09:00 -0500

    sincronizandome con los compañeros

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ class Main{
         System.out.print("Hola soy, Alejandro Rodríguez Duque");
 
         System.out.print("hola soy, andres santiago giraldo");
+
+        System.out.print("hola soy, Juan Diego Salas");
     }
 }
